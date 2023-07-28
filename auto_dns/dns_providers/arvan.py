14168d24678b1ed55bfe9f6808e7d7ba6b9c6124
Ali Tavallaie <a.tavallaie@gmail.com>
2023-07-29 00:09:19 +0330

    fix(arvan): handle optional record_type
    This update modifies the Arvan provider class to handle the situation when record_type is optional. Previously, the code would throw an error if no record_type was provided. Now, it handles this situation gracefully.

--- auto_dns/dns_providers/arvan.py
+++ auto_dns/dns_providers/arvan.py
@@ -1,21 +1,76 @@
-from .base import DNSProvider
+import requests
+from tabulate import tabulate
+
+API_URL = "https://napi.arvancloud.ir/cdn/4.0/domains/{domain}/dns-records"
 
 
-class Arvan(DNSProvider):
-    base_url = "https://napi.arvancloud.com/cdn/4.0/domains"
+class Arvan:
+    def __init__(self, api_key):
+        self.api_key = api_key
 
-    def get_record(self, domain, record_type):
-        # Implement Arvan's specific API calls
-        pass
+    def get_record(self, domain, record_type=None, subdomain=None):
+        response = requests.get(
+            API_URL.format(domain=domain),
+            headers={"Authorization": f"Apikey {self.api_key}"},
+        )
+        response.raise_for_status()
+        data = response.json()
+
+        # if record_type is provided, filter by type
+        if record_type:
+            print(f"Searching for records of type: {record_type}")
+            data["data"] = [
+                item for item in data["data"] if item["type"] == record_type
+            ]
+
+        # if subdomain is provided, filter by name
+        if subdomain:
+            print(f"Searching for records with subdomain: {subdomain}")
+            data["data"] = [item for item in data["data"] if item["name"] == subdomain]
+
+        # if no records found, return None
+        if not data["data"]:
+            print("No matching records found.")
+            return None
+
+        return data["data"]
 
     def create_record(self, record):
-        # Implement Arvan's specific API calls
-        pass
+        response = requests.post(
+            API_URL.format(domain=record.domain),
+            headers={"Authorization": f"Apikey {self.api_key}"},
+            json={
+                "type": record.record_type,
+                "value": record.ip,
+                "name": "",
+            },
+        )
+        response.raise_for_status()
+        return response.json()
 
     def update_record(self, record):
-        # Implement Arvan's specific API calls
-        pass
+        existing_record = self.get_record(record.domain, record.record_type)
+        if existing_record:
+            response = requests.put(
+                API_URL.format(domain=record.domain) + f"/{existing_record['id']}",
+                headers={"Authorization": f"Apikey {self.api_key}"},
+                json={
+                    "type": record.record_type,
+                    "value": record.ip,
+                    "name": "",
+                },
+            )
+            response.raise_for_status()
+            return response.json()
+        return None
 
     def delete_record(self, record):
-        # Implement Arvan's specific API calls
-        pass
+        existing_record = self.get_record(record.domain, record.record_type)
+        if existing_record:
+            response = requests.delete(
+                API_URL.format(domain=record.domain) + f"/{existing_record['id']}",
+                headers={"Authorization": f"Apikey {self.api_key}"},
+            )
+            response.raise_for_status()
+            return response.json()
+        return None
